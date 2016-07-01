e0b44ee588ca869a94bb59e65c2b13abc4c4a46f
Edward Lee <edwardlee0323@gmail.com>
2016-07-01 16:33:18 +0000

    PSQL db

--- nbastats/config.py
+++ nbastats/config.py
@@ -1,5 +1,5 @@
 class DevelopmentConfig(object):
-    DATABASE_URI = "postgresql://ubuntu:thinkful@localhost:5432/nbastats"
+    DATABASE_URI = "postgres://zmdkdpsjliuemk:NLy9RzlvAtKyw5OZ50Xn7neP4P@ec2-23-21-148-9.compute-1.amazonaws.com:5432/d3gntcc7ejvold"
     DEBUG = True
 
 class TestingConfig(object):
